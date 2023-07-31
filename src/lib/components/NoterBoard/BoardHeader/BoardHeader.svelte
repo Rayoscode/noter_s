7914711b86cbd3570398989651fd543b5a1bb61a
Martin Ramos <martin.ramos@innovativedev.com>
2023-07-31 18:23:57 -0300

    working horizontal resizable

--- src/lib/components/NoterBoard/BoardHeader/BoardHeader.svelte
+++ src/lib/components/NoterBoard/BoardHeader/BoardHeader.svelte
@@ -1,101 +1,122 @@
 <script>
-  import {fade} from "svelte/transition";
+  import { fade } from "svelte/transition";
   import BannerEditOptions from "$lib/components/NoterBoard/BoardHeader/BannerEditOptions/BannerEditOptions.svelte";
   import Resizable from "$lib/components/Resizable.svelte";
   //  Mock Data
   let headerData = {
-    projectName:"Proyectos 1",
-    iconUrl:"",
-    imageCSSProps:{
-      backgroundUrl:"url(https://wallpaperaccess.com/full/3497712.jpg)",
-      backgroundPosition:"center"
+    projectName: "Proyectos 1",
+    iconUrl: "",
+    imageCSSProps: {
+      backgroundUrl: "url(https://wallpaperaccess.com/full/3497712.jpg)",
+      backgroundPosition: "center",
     },
-    positionXBanner:"60",
-    positionYBanner:"60",
-    headerHeight:300,
-    headerMinHeight:200,
-  }
+    positionXBanner: "60",
+    positionYBanner: "60",
+    headerHeight: 300,
+    headerMinHeight: 200,
+  };
   // TODO: Cropping operation for bg img with background-position
-  let bannerWidth = 400;
-  function onHorizontalResize(event){
-    if(event.buttons === 1 ){
 
-      bannerWidth += event.movementX
-      console.log(bannerWidth)
-    }
-  }
+  let bannerSizes = { height: 150, width: 400 };
   let bannerOptionsVisible = false;
+  console.log("bannerSizes", bannerSizes);
 </script>
 
 <header>
-  <div class="background-header" style="min-height: {headerData.headerMinHeight}px; height:{headerData.headerHeight}px;">
-  {#if headerData.backgroundImage !== ""}
-<!--      <img on:error={errorImageHandle} src={headerData.backgroundImage} style="height:{headerData.headerHeight}px" alt="Header Background Image"/>-->
-    <div style="background: {headerData.imageCSSProps.backgroundUrl};
+  <div
+    class="background-header"
+    style="min-height: {headerData.headerMinHeight}px; height:{headerData.headerHeight}px;"
+  >
+    {#if headerData.imageCSSProps.backgroundUrl !== ""}
+      <!--      <img on:error={errorImageHandle} src={headerData.backgroundImage} style="height:{headerData.headerHeight}px" alt="Header Background Image"/>-->
+      <div
+        style="background: {headerData.imageCSSProps.backgroundUrl};
     height:{headerData.headerHeight}px;
     background-position:{headerData.imageCSSProps.backgroundPosition};
-    background-size:cover"></div>
-  {/if}
+    background-size:cover"
+      />
+    {/if}
   </div>
 
-
-<div on:mouseenter={()=>{bannerOptionsVisible = true}} on:mouseleave={()=>{bannerOptionsVisible = false}} class="title-banner"  style="top: {headerData.positionXBanner}px; left:{headerData.positionYBanner}px; width:{bannerWidth}px">
-  <div>
-    <Resizable onHorizontalResize={onHorizontalResize} width={bannerWidth} height="100%" resizableHorizontal={true}>
-      {#if bannerOptionsVisible}
-        <div in:fade={{duration:200,delay:100}} out:fade={{duration:200, delay:100}} class="option-banner-container">
-          <BannerEditOptions/>
+  <Resizable
+    size={bannerSizes}
+    position={{ x: 60, y: 60 }}
+    resizableHorizontal={true}
+  >
+    <div
+      on:mouseenter={() => {
+        bannerOptionsVisible = true;
+      }}
+      on:mouseleave={() => {
+        bannerOptionsVisible = false;
+      }}
+      role="banner"
+      class="title-banner"
+    >
+      <div>
+        {#if bannerOptionsVisible}
+          <div
+            in:fade={{ duration: 200, delay: 100 }}
+            out:fade={{ duration: 200, delay: 100 }}
+            class="option-banner-container"
+          >
+            <BannerEditOptions />
+          </div>
+        {/if}
+        <div class="title-container">
+          <input type="text" value={headerData.projectName} />
         </div>
-      {/if}
-      <input type="text" value={headerData.projectName}/>
-    </Resizable>
-  </div>
-</div>
+      </div>
+    </div>
+  </Resizable>
 </header>
 
 <style lang="scss">
-  header{
+  header {
     width: 100%;
   }
-  input{
-    appearance: none;
-    background-color: transparent;
-    outline: none;
-    font-size: x-large;
-    padding: 15px 30px 15px 10px;
-    width: 90%;
-    border:none;
-    caret-color: white;
-    color: white;
-    &:focus{
+  .title-container {
+    display: flex;
+    flex-direction: row;
+
+    input {
+      appearance: none;
+      background-color: transparent;
       outline: none;
+      font-size: x-large;
+      margin: 15px 10px 15px 10px;
+      width: 100%;
+      border: none;
+      caret-color: white;
+      color: white;
+      &:focus {
+        outline: none;
+      }
     }
   }
-  .title-banner{
+  .title-banner {
     border-radius: 8px;
-    position:absolute;
-    top:0;
-    left: 0;
-    background: rgba(98, 98, 98, 0.40);
+    background: rgba(98, 98, 98, 0.4);
     backdrop-filter: blur(4.5px);
+    width: 100%;
     display: inline-block;
-    > div{
+    > div {
       position: relative;
-      .option-banner-container{
-        width: 100%;
+      .option-banner-container {
+        width: 90%;
         position: absolute;
         overflow: hidden;
         padding-top: 10px;
         top: 55px;
         left: 0;
-        color:white;
+        color: white;
       }
     }
   }
-  .background-header{
+  .background-header {
     width: 100%;
     max-height: 700px;
-    >div{
+    > div {
       width: 100%;
     }
   }
--- src/lib/components/Resizable.svelte
+++ src/lib/components/Resizable.svelte
@@ -1,115 +1,198 @@
 <script>
-  /**
-   @type {number}*/
- export let width;
-  /**
-   @type {number}*/
-  export let height;
- export let resizableEdges = false;
- export let onHorizontalResize;
- export let onVerticalResize;
- export let onEdgeResize;
- export let resizableHorizontal = true;
- export let resizableVertical = false;
- let mousePressed = false;
-  window.removeEventListener()
+  import { onMount } from "svelte";
+  // props
+  // ---
+  /** @type { { width:number,height:number} }*/
+  export let size;
+  /** @type { { x:number,y:number} }*/
+  export let position;
+  export let resizableEdges = false;
+  export let resizableHorizontal = true;
+  export let resizableVertical = false;
+  // export let maxHeight = 100;
+  export let maxWidth = 800;
+  export let resizableLeft = true;
+  export let resizableRight = true;
+  export let resizableTop = false;
+  export let resizableBottom = false;
+  // export let resizableEdgeTopRight = false;
+  // export let resizableEdgeTopLeft = false;
+  // export let resizableEdgeBottomRight = false;
+  // export let resizableEdgeBottomLeft = false;
+  // ---
+  let minWidth = 150;
+  let mousePressed = false;
+
+  /** @type {(event:MouseEvent)=>void}*/
+  function onRightResize(event) {
+    if (
+      ((position.x + size.width + 10 > event.view?.innerWidth ||
+        size.width >= maxWidth) &&
+        event.movementX > 0) ||
+      (minWidth >= size.width && event.movementX < 0)
+    ) {
+      event.preventDefault();
+      return;
+    }
+    size.width += event.movementX;
+  }
+  /** @type {(event:MouseEvent)=>void}*/
+  function onLeftResize(event) {
+    if (
+      (size.width >= maxWidth && event.movementX < 0) ||
+      (minWidth >= size.width && event.movementX > 0)
+    ) {
+      event.preventDefault();
+      return;
+    }
+    position.x += event.movementX;
+    size.width -= event.movementX;
+  }
+  /** @type {(event:MouseEvent)=>void}*/
+  function onTopResize(event) {}
+  /** @type {(event:MouseEvent)=>void}*/
+  function onBottomResize() {}
+
+  /** @type {(event:MouseEvent)=>void}*/
+  function onResizeEnd(event) {
+    if (mousePressed) {
+      console.log(event);
+      if (resizableRight)
+        event.view?.removeEventListener("mousemove", onRightResize);
+      if (resizableLeft)
+        event.view?.removeEventListener("mousemove", onLeftResize);
+      if (resizableTop)
+        event.view?.removeEventListener("mousemove", onTopResize);
+      if (resizableBottom)
+        event.view?.removeEventListener("mousemove", onLeftResize);
+
+      mousePressed = false;
+      event.view?.removeEventListener("mouseup", onResizeEnd);
+    }
+  }
+
+  /** @type {(event:MouseEvent)=>void}*/
+  function onMouseDownResizeRight(event) {
+    mousePressed = true;
+    event.view?.window.addEventListener("mousemove", onRightResize);
+    event.view?.addEventListener("mouseup", onResizeEnd);
+    event.preventDefault();
+  }
+  /** @type {(event:MouseEvent)=>void}*/
+  function onMouseDownResizeLeft(event) {
+    mousePressed = true;
+    event.view?.window.addEventListener("mousemove", onLeftResize);
+    event.view?.addEventListener("mouseup", onResizeEnd);
+    event.preventDefault();
+  }
 </script>
 
-<div class="resizable-container">
-  {#if resizableVertical }
-    <div draggable="false"  on:mousemove={onVerticalResize} class="resizable-top"></div>
-    <div draggable="false"  on:mousemove={onVerticalResize} class="resizable-bottom"></div>
+<div
+  class="resizable-container"
+  style="width:{size.width}px; height:{size.height}px ;top:{position.y}px; left:{position.x}px"
+>
+  <div>
+    {#if resizableVertical}
+      <div class="resizable-top" />
+      <div class="resizable-bottom" />
+    {/if}
+    {#if resizableHorizontal || resizableLeft}
+      <div
+        on:mousedown={onMouseDownResizeLeft}
+        role="none"
+        class="resizable-left"
+      />
+    {/if}
+    {#if resizableHorizontal || resizableRight}
+      <div
+        on:mousedown={onMouseDownResizeRight}
+        role="none"
+        style="left: {size.width - 8}px"
+        class="resizable-right"
+      />
+    {/if}
+    <slot />
+  </div>
+  <!-- 
+  {#if resizableEdges}
+    <div
+      draggable="false"
 
-  {/if}
-  {#if resizableHorizontal }
-    <div draggable="false"
-         on:drag={(event)=>{event.stopImmediatePropagation(); event.preventDefault(); return false;}}
-         on:mousemove={onHorizontalResize}
-
-         class="resizable-left">
-    </div>
-    <div draggable="false"
-         on:drag={(event)=>{event.stopImmediatePropagation(); event.preventDefault(); return false;}}
-         on:mousemove={onHorizontalResize}
-         on:mouseup={()=>{mousePressed=false}}
-         on:mousedown={()=>{mousePressed = true}}
-
-         style="left: {width-8}px"
-         class="resizable-right">
-    </div>
-  {/if}
-
-  {#if resizableEdges }
-    <div draggable="false" on:copy={()=>{return false}} on:mousemove={onEdgeResize} class="resizable-left-diagonal"></div>
-    <div  draggable="false" on:copy={()=>{return false}} on:mousemove={onEdgeResize} class="resizable-right-diagonal"  ></div>
-  {/if}
-  <slot></slot>
-
+      on:mousemove={onEdgeResize}
+      class="resizable-left-diagonal"
+    />
+    <div
+      draggable="false"
+      on:copy={() => {
+        return false;
+      }}
+      on:mousemove={onEdgeResize}
+      class="resizable-right-diagonal"
+    />
+  {/if} -->
 </div>
 
 <style lang="scss">
-  .resizable-container{
-    position: relative;
+  .resizable-container {
+    position: absolute;
     top: 0;
     width: inherit;
     height: inherit;
     left: 0;
-
+    > div {
+      position: relative;
+    }
   }
-  .resizable-right{
+  .resizable-right {
     cursor: e-resize;
-    background: red;
-
+    background: transparent;
+    z-index: 10;
     height: 100%;
-    width: 20px;
+    width: 10px;
     position: absolute;
     top: 0;
     left: 0px;
   }
-  .resizable-left{
-    background: red;
-
+  .resizable-left {
+    background: transparent;
+    z-index: 10;
     cursor: e-resize;
     height: 100%;
-    width: 8px;
+    width: 10px;
     position: absolute;
     top: 0;
-    left: 1px;
+    left: 0px;
   }
-  .resizable-left-diagonal{
+  .resizable-left-diagonal {
     width: 10px;
     height: 10px;
     cursor: ne-resize;
     position: absolute;
     top: 0;
     left: 0;
-
   }
-  .resizable-top{
+  .resizable-top {
     background: red;
     position: absolute;
-    top:0;
+    top: 0;
     left: 0;
     width: 100%;
     height: 8px;
     cursor: n-resize;
     z-index: -1;
-
   }
-  .resizable-bottom{
+  .resizable-bottom {
     background: red;
     width: 100%;
     height: 8px;
     position: absolute;
-    top:0;
+    top: 0;
     left: 0;
     cursor: n-resize;
     z-index: -1;
-
   }
-  .resizable-right-diagonal{
+  .resizable-right-diagonal {
     cursor: ns-resize;
     z-index: -1;
-
   }
 </style>
